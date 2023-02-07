d6492afd071a1014a76ed65d3380cea7a9c028c9
Antovero <dosaeun@gmail.com>
2023-02-07 21:01:47 +0900

    darkmode save localStorage

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,11 +5,12 @@ import { MdDarkMode } from 'react-icons/md';
 import { CgSun } from 'react-icons/cg';
 
 export default function Header({filters, filter, onFilterChange}) {
-    const {darkMode, handleDarkMode} = useDarkMode();
+    const {darkMode, toggleDarkMode} = useDarkMode();
     return (
         <header className={styles.header}>
-            <div className={styles.themeBtn} onClick={handleDarkMode}>{
-                darkMode === true ? <MdDarkMode/> : <CgSun/> }</div>
+            <button type="button" className={styles.themeBtn} onClick={toggleDarkMode}>{
+                darkMode === true ? <MdDarkMode/> : <CgSun/> }
+            </button>
             <ul className={styles.filters}>
                 {filters.map((value, index)=><li key={index}>
                     <button className={`${styles.filter} ${filter === value && styles.selected}`} onClick={()=>onFilterChange(value)}>{value}</button>
